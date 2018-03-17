51b09c67f9fd9b3e42e1419b529abb5553277c52
Harsh Sahu <sahuharsh96@gmail.com>
2018-03-17 18:56:12 +0530

    Add files via upload

--- prepare_input.py
+++ prepare_input.py
@@ -3,31 +3,29 @@ import os, os.path
 import numpy as np
 import cv2
 from keras.utils import np_utils
+from scipy import misc
 
-images_path = os.path.join(os.path.dirname(__file__), 'images')
+images_path = os.path.join(os.path.dirname(__file__), 'images2')
 
 def prepare_input():
 		start = time.time()
-		print('preparing input...')
 		data = []
 		label = []
 		folder_no = 0
 		for folder in os.listdir(images_path):
 			class_images_path = images_path+'/'+folder
-			num_example = len([name for name in os.listdir(class_images_path)])
-			print(class_images_path)
-			print(num_example)
-			for i in range(1000,2000):
-				if i%10==0:
-					image_array = cv2.imread(class_images_path+'/frame'+str(i)+'.jpg')
-					#data.append(image_array)
-					resized_image_array = cv2.resize(image_array, (299, 299))
-					data.append(resized_image_array)
-					label.append(folder_no)
-		folder_no+=1
+			print('Moving to: '+ class_images_path)
+			num_example = len([image for image in os.listdir(class_images_path)])
+			for image in os.listdir(class_images_path):
+				print('Creating array for'+class_images_path+'/'+image+'...')
+				image_array = cv2.imread(class_images_path+'/'+image)
+				#data.append(image_array)
+				resized_image_array = cv2.resize(image_array, (299, 299))
+				data.append(resized_image_array)
+				label.append(folder_no)
+			folder_no+=1
 		data = np.array(data)
 		label = np.array(label)
-		print(data.shape)
 		
 		p = np.random.permutation(data.shape[0])
 		X = data[p]
@@ -48,10 +46,10 @@ def prepare_input():
 		#print(y_train[:10], y_test[:10])
 		print("Time taken for creating numpy arrays: ", time.time() - start)
 		
-		np.save('./numpy_arrays/X_train2.npy', X_train)
-		np.save('./numpy_arrays/y_train2.npy', y_train_vector)
-		np.save('./numpy_arrays/X_test2.npy', X_test)
-		np.save('./numpy_arrays/y_test2.npy', y_test_vector)
-
+		np.save('./numpy_arrays/X_train_images2_inception.npy', X_train)
+		np.save('./numpy_arrays/y_train_images2_inception.npy', y_train_vector)
+		np.save('./numpy_arrays/X_test_images2_inception.npy', X_test)
+		np.save('./numpy_arrays/y_test_images2_inception.npy', y_test_vector)
+		
 if __name__ == '__main__':
 	prepare_input()
